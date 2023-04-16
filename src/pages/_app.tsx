b8ca164b38dc12d2072761fde718b4020e19d8cc
Dornemann, Nino <nino.dornemann@interhyp.de>
2023-04-16 08:46:06 +0200

    [ODCC-1] add prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  let dwadaw = 'dada'
+  console.log(dwadaw)
+  return <Component {...pageProps} />
 }
